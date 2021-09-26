dd9278aa45deaf7e1052b6f8fd83a71ceef1bb64
abelramadhan <64635958+abelramadhan@users.noreply.github.com>
2021-09-26 18:32:55 +0700

    Add project files.

---
+++ Demo.cpp
@@ -0,0 +1,503 @@
+#include "Demo.h"
+
+
+
+Demo::Demo() {
+
+}
+
+
+Demo::~Demo() {
+}
+
+
+
+void Demo::Init() {
+	// build and compile our shader program
+	// ------------------------------------
+	shaderProgram = BuildShader("vertexShader.vert", "fragmentShader.frag", nullptr);
+
+	BuildBlade();
+	BuildGuard();
+	BuildHandle();
+
+	InitCamera();
+}
+
+void Demo::DeInit() {
+	// optional: de-allocate all resources once they've outlived their purpose:
+	// ------------------------------------------------------------------------
+	glDeleteVertexArrays(1, &VAO);
+	glDeleteBuffers(1, &VBO);
+	glDeleteBuffers(1, &EBO);
+}
+
+// process all input: query GLFW whether relevant keys are pressed/released this frame and react accordingly
+// ---------------------------------------------------------------------------------------------------------
+void Demo::ProcessInput(GLFWwindow* window) {
+	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
+		glfwSetWindowShouldClose(window, true);
+	}
+
+	// zoom camera
+	// -----------
+	if (glfwGetMouseButton(window, GLFW_MOUSE_BUTTON_RIGHT) == GLFW_PRESS) {
+		if (fovy < 90) {
+			fovy += 0.0001f;
+		}
+	}
+
+	if (glfwGetMouseButton(window, GLFW_MOUSE_BUTTON_LEFT) == GLFW_PRESS) {
+		if (fovy > 0) {
+			fovy -= 0.0001f;
+		}
+	}
+
+	// update camera movement 
+	// -------------
+	if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS) {
+		MoveCamera(CAMERA_SPEED);
+	}
+	if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS) {
+		MoveCamera(-CAMERA_SPEED);
+	}
+
+	if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS) {
+		StrafeCamera(-CAMERA_SPEED);
+	}
+
+	if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS) {
+		StrafeCamera(CAMERA_SPEED);
+	}
+
+	// update camera rotation
+	// ----------------------
+	double mouseX, mouseY;
+	double midX = screenWidth / 2;
+	double midY = screenHeight / 2;
+	float angleY = 0.0f;
+	float angleZ = 0.0f;
+
+	// Get mouse position
+	glfwGetCursorPos(window, &mouseX, &mouseY);
+	if ((mouseX == midX) && (mouseY == midY)) {
+		return;
+	}
+
+	// Set mouse position
+	glfwSetCursorPos(window, midX, midY);
+
+	// Get the direction from the mouse cursor, set a resonable maneuvering speed
+	angleY = (float)((midX - mouseX)) / 1000;
+	angleZ = (float)((midY - mouseY)) / 1000;
+
+	// The higher the value is the faster the camera looks around.
+	viewCamY += angleZ * 2;
+
+	// limit the rotation around the x-axis
+	if ((viewCamY - posCamY) > 8) {
+		viewCamY = posCamY + 8;
+	}
+	if ((viewCamY - posCamY) < -8) {
+		viewCamY = posCamY - 8;
+	}
+	RotateCamera(-angleY);
+
+
+}
+
+void Demo::Update(double deltaTime) {
+
+}
+
+void Demo::InitCamera()
+{
+	posCamX = 0.0f;
+	posCamY = 0.4f;
+	posCamZ = 2.0f;
+	viewCamX = 0.0f;
+	viewCamY = 0.1f;
+	viewCamZ = 0.0f;
+	upCamX = 0.0f;
+	upCamY = 1.0f;
+	upCamZ = 0.0f;
+	CAMERA_SPEED = 0.0001f;
+	fovy = 45.0f;
+	glfwSetInputMode(this->window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+}
+
+
+void Demo::MoveCamera(float speed)
+{
+	float x = viewCamX - posCamX;
+	float z = viewCamZ - posCamZ;
+	// forward positive cameraspeed and backward negative -cameraspeed.
+	posCamX = posCamX + x * speed;
+	posCamZ = posCamZ + z * speed;
+	viewCamX = viewCamX + x * speed;
+	viewCamZ = viewCamZ + z * speed;
+}
+
+void Demo::StrafeCamera(float speed)
+{
+	float x = viewCamX - posCamX;
+	float z = viewCamZ - posCamZ;
+	float orthoX = -z;
+	float orthoZ = x;
+
+	// left positive cameraspeed and right negative -cameraspeed.
+	posCamX = posCamX + orthoX * speed;
+	posCamZ = posCamZ + orthoZ * speed;
+	viewCamX = viewCamX + orthoX * speed;
+	viewCamZ = viewCamZ + orthoZ * speed;
+}
+
+void Demo::RotateCamera(float speed)
+{
+	float x = viewCamX - posCamX;
+	float z = viewCamZ - posCamZ;
+	viewCamZ = (float)(posCamZ + glm::sin(speed) * x + glm::cos(speed) * z);
+	viewCamX = (float)(posCamX + glm::cos(speed) * x - glm::sin(speed) * z);
+}
+
+void Demo::Render() {
+
+	glViewport(0, 0, this->screenWidth, this->screenHeight);
+
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	glClearColor(0.07f, 0.13f, 0.17f, 1.0f);
+
+	glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+
+	//glEnable(GL_CULL_FACE);
+
+	glEnable(GL_DEPTH_TEST);
+
+	// Pass perspective projection matrix
+	glm::mat4 projection = glm::perspective(fovy, (GLfloat)this->screenWidth / (GLfloat)this->screenHeight, 0.1f, 100.0f);
+	GLint projLoc = glGetUniformLocation(this->shaderProgram, "projection");
+	glUniformMatrix4fv(projLoc, 1, GL_FALSE, glm::value_ptr(projection));
+
+	// LookAt camera (position, target/direction, up)
+	glm::mat4 view = glm::lookAt(glm::vec3(posCamX, posCamY, posCamZ), glm::vec3(viewCamX, viewCamY, viewCamZ), glm::vec3(upCamX, upCamY, upCamZ));
+	GLint viewLoc = glGetUniformLocation(this->shaderProgram, "view");
+	glUniformMatrix4fv(viewLoc, 1, GL_FALSE, glm::value_ptr(view));
+
+	DrawBlade();
+	DrawGuard();
+	DrawHandle();
+
+	glDisable(GL_DEPTH_TEST);
+}
+
+void Demo::BuildBlade() {
+	// load image into texture memory
+	// ------------------------------
+	// Load and create a texture 
+	glGenTextures(1, &texture);
+	glBindTexture(GL_TEXTURE_2D, texture);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+	int width, height;
+	unsigned char* image = SOIL_load_image("metal2.png", &width, &height, 0, SOIL_LOAD_RGBA);
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, image);
+	glGenerateMipmap(GL_TEXTURE_2D);
+	SOIL_free_image_data(image);
+	glBindTexture(GL_TEXTURE_2D, 0);
+
+	// set up vertex data (and buffer(s)) and configure vertex attributes
+	// ------------------------------------------------------------------
+	float vertices[] = {
+		// format position, tex coords
+		0.03f,		0.9f,	0.0f,		1, 1,	//0
+		0.03f,		0.6f,	0.015f,		1, 0,	//1
+		-0.04f,		0.7f,	0.0f,		0, 1,	//2
+		-0.025f,	0.0f,	0.0f,		0, 0,	//3
+		0.03f,		0.0f,	0.015f,		1, 0,	//4
+		0.03f,		0.6f,	-0.015f,	1, 0.5,	//5
+		0.03f,		0.0f,	-0.015f,	1, 0,	//6
+
+		0.03f,		0.9f,	0.0f,		0.5, 1,	//7
+		0.03f,		0.6f,	0.015f,		0, 1,	//8
+		0.03f,		0.6f,	-0.015f,	1, 1,	//9
+		0.03f,		0.0f,	0.015f,		0, 0,	//10
+		0.03f,		0.0f,	-0.015f,	1, 0,	//11
+	};
+
+	unsigned int indices[] = {
+		0,1,2,
+		2,1,3,
+		1,3,4,
+		0,5,2,
+		2,5,3,
+		5,3,6,
+		7,8,9,
+		8,10,11,
+		8,9,11,
+	};
+
+	glGenVertexArrays(1, &VAO);
+	glGenBuffers(1, &VBO);
+	glGenBuffers(1, &EBO);
+	// bind the Vertex Array Object first, then bind and set vertex buffer(s), and then configure vertex attributes(s).
+	glBindVertexArray(VAO);
+
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+	// define position pointer layout 0
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(GLfloat), (GLvoid*)(0 * sizeof(GLfloat)));
+	glEnableVertexAttribArray(0);
+
+	// define texcoord pointer layout 1
+	glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(GLfloat), (GLvoid*)(3 * sizeof(GLfloat)));
+	glEnableVertexAttribArray(1);
+
+	// note that this is allowed, the call to glVertexAttribPointer registered VBO as the vertex attribute's bound vertex buffer object so afterwards we can safely unbind
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+	// You can unbind the VAO afterwards so other VAO calls won't accidentally modify this VAO, but this rarely happens. Modifying other
+	// VAOs requires a call to glBindVertexArray anyways so we generally don't unbind VAOs (nor VBOs) when it's not directly necessary.
+	glBindVertexArray(0);
+
+	// remember: do NOT unbind the EBO while a VAO is active as the bound element buffer object IS stored in the VAO; keep the EBO bound.
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+
+}
+
+void Demo::DrawBlade()
+{
+	glUseProgram(shaderProgram);
+
+	glActiveTexture(GL_TEXTURE0);
+	glBindTexture(GL_TEXTURE_2D, texture);
+	glUniform1i(glGetUniformLocation(this->shaderProgram, "ourTexture"), 0);
+
+	glBindVertexArray(VAO); // seeing as we only have a single VAO there's no need to bind it every time, but we'll do so to keep things a bit more organized
+
+	glDrawElements(GL_TRIANGLE_STRIP, 27, GL_UNSIGNED_INT, 0);
+
+	glBindTexture(GL_TEXTURE_2D, 0);
+	glBindVertexArray(0);
+
+}
+
+void Demo::BuildGuard() {
+	// load image into texture memory
+	// ------------------------------
+	// Load and create a texture 
+	glGenTextures(1, &texture2);
+	glBindTexture(GL_TEXTURE_2D, texture2);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_MIRRORED_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_MIRRORED_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	int width, height;
+	unsigned char* image = SOIL_load_image("wood2.png", &width, &height, 0, SOIL_LOAD_RGBA);
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, image);
+	glGenerateMipmap(GL_TEXTURE_2D);
+	SOIL_free_image_data(image);
+	glBindTexture(GL_TEXTURE_2D, 0);
+
+	// set up vertex data (and buffer(s)) and configure vertex attributes
+	// ------------------------------------------------------------------
+	float vertices[] = {
+		// format position, tex coords
+		-0.04f,		0.0f,		0.02f,		0, 0,		//0
+		0.045f,		0.0f,		0.03f,		2, 0,		//1
+		0.045f,		0.0f,		-0.03f,		2, 2,		//2
+		-0.04f,		0.0f,		-0.02f,		0, 2,		//3
+		-0.04f,		-0.02f,		0.02f,		0, 0,		//4
+		0.045f,		-0.02f,		0.03f,		2, 0,		//5
+		0.045f,		-0.02f,		-0.03f,		2, 2,		//6
+		-0.04f,		-0.02f,		-0.02f,		0, 2,		//7
+		
+		-0.026f,	-0.18f,		0.018f,		0, 0,		//8
+		0.035f,		-0.18f,		0.018f,		2, 0,		//9
+		0.035f,		-0.18f,		-0.018f,	2, 2,		//10
+		-0.026f,	-0.18f,		-0.018f,	0, 2,		//11
+		-0.026f,	-0.19f,		0.018f,		0, 0,		//12
+		0.035f,		-0.19f,		0.018f,		2, 0,		//13
+		0.035f,		-0.19f,		-0.018f,	2, 2,		//14
+		-0.026f,	-0.19f,		-0.018f,	0, 2,		//15
+
+	};
+
+	unsigned int indices[] = {
+		0,1,2,0,2,3,
+		0,4,5,0,1,5,
+		4,5,6,4,7,6,
+		3,2,6,3,7,6,
+		3,0,4,3,7,4,
+		2,1,5,2,6,5,
+
+		8,9,10,8,10,11,
+		11,14,10,11,14,15,
+		15,12,14,13,12,14,
+		12,13,9,12,8,9,
+		11,8,12,12,15,8,
+		14,9,10,14,9,13
+	};
+
+	glGenVertexArrays(1, &VAO2);
+	glGenBuffers(1, &VBO2);
+	glGenBuffers(1, &EBO2);
+	// bind the Vertex Array Object first, then bind and set vertex buffer(s), and then configure vertex attributes(s).
+	glBindVertexArray(VAO2);
+
+	glBindBuffer(GL_ARRAY_BUFFER, VBO2);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO2);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+	// define position pointer layout 0
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(GLfloat), (GLvoid*)(0 * sizeof(GLfloat)));
+	glEnableVertexAttribArray(0);
+
+	// define texcoord pointer layout 1
+	glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(GLfloat), (GLvoid*)(3 * sizeof(GLfloat)));
+	glEnableVertexAttribArray(1);
+
+	// note that this is allowed, the call to glVertexAttribPointer registered VBO as the vertex attribute's bound vertex buffer object so afterwards we can safely unbind
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+	// You can unbind the VAO afterwards so other VAO calls won't accidentally modify this VAO, but this rarely happens. Modifying other
+	// VAOs requires a call to glBindVertexArray anyways so we generally don't unbind VAOs (nor VBOs) when it's not directly necessary.
+	glBindVertexArray(0);
+
+	// remember: do NOT unbind the EBO while a VAO is active as the bound element buffer object IS stored in the VAO; keep the EBO bound.
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+
+}
+
+void Demo::DrawGuard()
+{
+	glUseProgram(shaderProgram);
+
+	glActiveTexture(GL_TEXTURE1);
+	glBindTexture(GL_TEXTURE_2D, texture2);
+	glUniform1i(glGetUniformLocation(this->shaderProgram, "ourTexture"), 1);
+
+	glBindVertexArray(VAO2); // seeing as we only have a single VAO there's no need to bind it every time, but we'll do so to keep things a bit more organized
+
+	glDrawElements(GL_TRIANGLES, 72, GL_UNSIGNED_INT, 0);
+
+	glBindTexture(GL_TEXTURE_2D, 0);
+	glBindVertexArray(0);
+
+}
+
+void Demo::BuildHandle() {
+	// load image into texture memory
+	// ------------------------------
+	// Load and create a texture 
+	glGenTextures(1, &texture3);
+	glBindTexture(GL_TEXTURE_2D, texture3);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	int width, height;
+	unsigned char* image = SOIL_load_image("leather.png", &width, &height, 0, SOIL_LOAD_RGBA);
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, image);
+	glGenerateMipmap(GL_TEXTURE_2D);
+	SOIL_free_image_data(image);
+	glBindTexture(GL_TEXTURE_2D, 0);
+
+	// set up vertex data (and buffer(s)) and configure vertex attributes
+	// ------------------------------------------------------------------
+	float vertices[] = {
+		// format position, tex coords
+		-0.02f,		-0.18f,		0.012f,		0, 0,	//0
+		0.028f,		-0.18f,		0.012f,		2, 0,	//1
+		0.028f,		-0.02f,		0.012f,		2, 2,	//2
+		-0.025f,	-0.02f,		0.012f,		0, 2,	//3
+
+		-0.02f,		-0.18f,		-0.012f,	0, 0,	//4
+		0.028f,		-0.18f,		-0.012f,	2, 0,	//5
+		0.028f,		-0.02f,		-0.012f,	2, 2,	//6
+		-0.025f,	-0.02f,		-0.012f,	0, 2,	//7
+
+		-0.02f,		-0.18f,		0.012f,		2, 0,	//8
+		-0.02f,		-0.18f,		-0.012f,	0, 0,	//9
+		-0.025f,	-0.02f,		0.012f,		2, 2,	//10
+		-0.025f,	-0.02f,		-0.012f,	0, 2,	//11
+
+		0.028f,		-0.18f,		0.012f,		0, 0,	//12
+		0.028f,		-0.18f,		-0.012f,	2, 0,	//13
+		0.028f,		-0.02f,		0.012f,		0, 2,	//14
+		0.028f,		-0.02f,		-0.012f,	2, 2	//15
+
+	};
+
+	unsigned int indices[] = {
+		0,1,2,
+		3,0,2,
+		4,5,6,
+		7,4,6,
+		8,10,9,
+		9,10,11,
+		12,13,14,
+		13,14,15,
+	};
+
+	glGenVertexArrays(1, &VAO3);
+	glGenBuffers(1, &VBO3);
+	glGenBuffers(1, &EBO3);
+	// bind the Vertex Array Object first, then bind and set vertex buffer(s), and then configure vertex attributes(s).
+	glBindVertexArray(VAO3);
+
+	glBindBuffer(GL_ARRAY_BUFFER, VBO3);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO3);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+	// define position pointer layout 0
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(GLfloat), (GLvoid*)(0 * sizeof(GLfloat)));
+	glEnableVertexAttribArray(0);
+
+	// define texcoord pointer layout 1
+	glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(GLfloat), (GLvoid*)(3 * sizeof(GLfloat)));
+	glEnableVertexAttribArray(1);
+
+	// note that this is allowed, the call to glVertexAttribPointer registered VBO as the vertex attribute's bound vertex buffer object so afterwards we can safely unbind
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+	// You can unbind the VAO afterwards so other VAO calls won't accidentally modify this VAO, but this rarely happens. Modifying other
+	// VAOs requires a call to glBindVertexArray anyways so we generally don't unbind VAOs (nor VBOs) when it's not directly necessary.
+	glBindVertexArray(0);
+
+	// remember: do NOT unbind the EBO while a VAO is active as the bound element buffer object IS stored in the VAO; keep the EBO bound.
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+
+}
+
+void Demo::DrawHandle ()
+{
+	glUseProgram(shaderProgram);
+
+	glActiveTexture(GL_TEXTURE2);
+	glBindTexture(GL_TEXTURE_2D, texture3);
+	glUniform1i(glGetUniformLocation(this->shaderProgram, "ourTexture"), 2);
+
+	glBindVertexArray(VAO3); // seeing as we only have a single VAO there's no need to bind it every time, but we'll do so to keep things a bit more organized
+
+	glDrawElements(GL_TRIANGLES, 36, GL_UNSIGNED_INT, 0);
+
+	glBindTexture(GL_TEXTURE_2D, 0);
+	glBindVertexArray(0);
+
+}
+
+int main(int argc, char** argv) {
+	RenderEngine &app = Demo();
+	app.Start("Sword 3D Model with Primitives", 1280, 720, false, false);
+}
